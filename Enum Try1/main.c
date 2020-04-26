b8ad4728d40bdfad49bac0fb9326d80ad931422d
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-04-26 13:57:32 +0200

    update

---
+++ Enum Try1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    enum gender {male, female};
+    enum age {myAge};
+
+    myAge = 23;
+    enum age anotherAge - 15;
+
+
+
+    return 0;
+}
